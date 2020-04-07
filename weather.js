d980076ea78e1f2c7b014d004f433db22e215956
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2020-04-06 22:44:15 -0400

    both functions are working. need to re-tweak the if statement

--- weather.js
+++ weather.js
@@ -29,18 +29,49 @@ function WeatherForecast(city) { //function for the weather forecast of the curr
 
         $("#card-body1").empty();
         var cardbodA = $("#card-body1").append(city, tempF, humid, windspeed);
-        
+
         $("#current-day").append(cardbodA);
     });
 
 }
 
-$("#SearchBtn").on("click", function(event) {
+function multipledayForecast(city) {
+    var APIkey = "&appid=801906cebc106515bac3bda6ad7c527b";
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + APIkey;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        var results = response.list
+        for (var i = 0; i < results.length; i++){
+            if (results[i].dt_txt.indexOf("12:00:00")){
+                var dailydiv = $("<div>")
+                var degrees = results[i].main.temp;
+                var degInKel = (degrees - 273.25) * 1.80 + 32;
+                var degInFaren = $("<h2>").text("Temperature (F): " + degInKel.toFixed(2));
+                var sweaty = $("<h3>").text("Humidity: " + results[i].main.humidity + " % ");
+                var windfast = $("<h4>").text("Wind Speed: " + results[i].wind.speed);
+
+                var dailydivA = dailydiv.append(degInFaren, sweaty, windfast);
+                
+                $(".container").append(dailydivA);
+            } 
+
+        }
+    });
+
+}
+
+$("#SearchBtn").on("click", function (event) {
     event.preventDefault();
 
     var cityinput = $("#Search-Term").val().trim();
 
     WeatherForecast(cityinput);
+
+    multipledayForecast(cityinput);
+
 
 });
 
