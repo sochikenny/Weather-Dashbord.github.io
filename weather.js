518c5e194d06326390c8b88f5e7ce2bdab2ae43d
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2020-04-06 21:07:16 -0400

    added CurrentDay function and SearchBtn on clicks

--- weather.js
+++ weather.js
@@ -1,41 +1,48 @@
 //$("button").on("click", function () {
 
 // var city = $(this).attr("aria-label");
-function searchcity(city) { }
-var city = "Atlanta";
+//function searchcity(city) { }
+//var city = "Atlanta";
+//var city = $("#SearchTerm").val("");
 
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" +
-    city + "&appid=801906cebc106515bac3bda6ad7c527b";
+function CurrentDay(city) {
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-    console.log(response.city.name);
-    console.log(response.list[0].main.temp);
-    console.log(response.list[0].main.humidity);
-    console.log(response.list[0].wind.speed);
+    var APIkey = "&appid=801906cebc106515bac3bda6ad7c527b";
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + APIkey;
 
-    var city = $("<h1>").text(response.city.name);
-    var temperature = response.list[0].main.temp;
-    var tempK = (temperature - 273.15) * 1.80 + 32;
-    var tempF = $("<h2>").text("Temperature (F): " + tempK.toFixed(2));
-    var humid = $("<h3>").text("Humidity: " + response.list[0].main.humidity + " % ");
-    var windspeed = $("<h4>").text("Wind Speed: " + response.list[0].wind.speed);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        console.log(response.city.name);
+        console.log(response.list[0].main.temp);
+        console.log(response.list[0].main.humidity);
+        console.log(response.list[0].wind.speed);
 
-    var cardbodA = $("#card-body1").append(city, tempF, humid, windspeed);
-    $("#current-day").append(cardbodA);
+        var city = $("<h1>").text(response.city.name);
+        var temperature = response.list[0].main.temp;
+        var tempK = (temperature - 273.15) * 1.80 + 32;
+        var tempF = $("<h2>").text("Temperature (F): " + tempK.toFixed(2));
+        var humid = $("<h3>").text("Humidity: " + response.list[0].main.humidity + " % ");
+        var windspeed = $("<h4>").text("Wind Speed: " + response.list[0].wind.speed);
 
+        var cardbodA = $("#card-body1").append(city, tempF, humid, windspeed);
+        $("#current-day").append(cardbodA);
+    });
 
+}
 
+$("#SearchBtn").on("click", function(event) {
+    event.preventDefault();
 
+    var cityinput = $("#Search-Term").val().trim();
 
-
-
-
+    CurrentDay(cityinput);
 
 });
+
+
 
 
 
