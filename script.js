82edfbcfea2a278e7590629cd845d5a5433d7c28
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2020-04-12 12:17:11 -0400

    changed file names. added weather icons. enabled a new functionality when user hits enter on the keyboard after typing city name.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-function CurrentDayForecast(city) { //function for the weather forecast of the current day in whatever city
+function CurrentDayForecast(city) {
 
     var APIkey = "&appid=801906cebc106515bac3bda6ad7c527b";
     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + APIkey;
@@ -8,21 +8,18 @@ function CurrentDayForecast(city) { //function for the weather forecast of the c
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
-        console.log(response.city.name);
-        console.log(response.list[0].main.temp);
-        console.log(response.list[0].main.humidity);
-        console.log(response.list[0].wind.speed);
-
+        
         var city = $("<h1>").text(response.city.name);
         var temperature = response.list[0].main.temp;
         var tempK = (temperature - 273.15) * 1.80 + 32;
-        var tempF = $("<h2>").text("Temperature (F): " + tempK.toFixed(0));
+        var tempF = $("<h3>").text("Temperature (F): " + tempK.toFixed(0));
         var humid = $("<h3>").text("Humidity: " + response.list[0].main.humidity + " % ");
-        var windspeed = $("<h4>").text("Wind Speed: " + response.list[0].wind.speed + " mph");
+        var windspeed = $("<h3>").text("Wind Speed: " + response.list[0].wind.speed + " mph");
+        var CurrentWeatherIcon = response.list[0].weather[0].icon;
+        var getCurrentWeatherIcon = $("<img src = http://openweathermap.org/img/wn/" + CurrentWeatherIcon + "@2x.png />");
 
-       
-        var cardbodA = $("#card-body1").append(city, tempF, humid, windspeed);
+
+        var cardbodA = $("#card-body1").append(city, getCurrentWeatherIcon, tempF, humid, windspeed);
 
         $("#current-day").append(cardbodA);
     });
@@ -38,27 +35,29 @@ function multipledayForecast(city) {
         method: "GET"
     }).then(function (response) {
         var results = response.list
-        for (var i = 0; i < results.length; i++){
-            if ((results[i].dt_txt.indexOf("12:00:00") !== -1)){
+        for (var i = 0; i < results.length; i++) {
+            if ((results[i].dt_txt.indexOf("12:00:00") !== -1)) {
                 var dailydiv = $('<div class="card col-md-2 ml-4 bg-primary text-white"></div>');
                 var subdailydiv = $('<div class="card-body p-3 forecastBody"></div>')
                 var degrees = results[i].main.temp;
                 var degInKel = Math.floor((degrees - 273.25) * 1.80 + 32);
                 var degInFaren = $("<h5>").text("Temperature (F): " + degInKel.toFixed(0));
                 var sweaty = $("<h6>").text("Humidity: " + results[i].main.humidity + " % ");
-                var windfast = $("<h7>").text("Wind Speed: " + Math.floor(results[i].wind.speed) + " mph");
+                var windfast = $("<h7>").text("Wind Speed: " + results[i].wind.speed + " mph");
+                var CurrentWeatherIcons = response.list[i].weather[0].icon;
+                var getCurrentWeatherIcons = $("<img src = http://openweathermap.org/img/wn/" + CurrentWeatherIcons + "@2x.png />")
 
-           
-                var dailydivA = subdailydiv.append(degInFaren, sweaty, windfast);
+
+                var dailydivA = subdailydiv.append(degInFaren, sweaty, windfast, getCurrentWeatherIcons);
                 var dailydivB = dailydiv.append(dailydivA);
-                
-                
+
+
                 var dailyforecast = $(".row").append(dailydivB);
-                
+
                 $(".container").append(dailyforecast);
-                
-                
-            } 
+
+
+            }
 
         }
     });
@@ -76,13 +75,11 @@ $("#SearchBtn").on("click", function (event) {
     multipledayForecast(cityinput);
     $(".row").empty();
 
-
 });
 
+$("#Search-Term").keypress(function(e){
+    if(e.which == 13){
+        $("#SearchBtn").click();
+    }
+});
 
-
-
-
-
-
-//});
